a93c5d1579cc0a9405fed724580cbb256b510f29
daminhoOPs <dami.adesanya@yahoo.com>
2023-05-24 23:21:49 +0300

    Ecommerce E2E commit - Hybrid TestNG Framework

--- src/test/java/com/tutorialsninja/qa/testcases/Login.java
+++ src/test/java/com/tutorialsninja/qa/testcases/Login.java
@@ -5,29 +5,72 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class Login {
+import com.tutorialsninja.qa.base.baseClass;
+
+public class Login extends baseClass {
+	WebDriver driver;
 	
-	@Test
+	@BeforeMethod
+	public void setup () {
+		
+	driver = initializeBrowserAndOpenAppURL("edge");
+	driver.findElement(By.xpath("//span[text()='My Account']")).click();
+	driver.findElement(By.linkText("Login")).click();
+	}
+	
+	@AfterMethod
+	public void	tearDown () {
+		driver.quit();
+	}
+	
+	@Test (priority=1)
 	public void verifyingLoginWithValidCredentials () {
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-		//driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
-		driver.get("http://www.tutorialsninja.com/demo/");
-		driver.findElement(By.xpath("//span[text()='My Account']")).click();
-		driver.findElement(By.linkText("Login")).click();
+
 		driver.findElement(By.id("input-email")).sendKeys("amotooricap9@gmail.com");
 		driver.findElement(By.id("input-password")).sendKeys("12345");
 		driver.findElement(By.xpath("//input[@value='Login']")).click();
 		
 		Assert.assertTrue(driver.findElement(By.linkText("Edit your account information")).isDisplayed());
-		driver.quit();
+		
 				
 	}
-	
-	
+	@Test (priority=2)
+	public void loginWithWrongDetails () {
+
+		driver.findElement(By.id("input-email")).sendKeys("amotooricap9@yahoomail.com");
+		driver.findElement(By.id("input-password")).sendKeys("ABC12345");
+		driver.findElement(By.xpath("//input[@value='Login']")).click();
+		
+		Assert.assertTrue(driver.findElement(By.xpath("//div[@class='alert alert-danger alert-dismissible']")).isDisplayed());
+		
+	}
+	@Test (priority=3)
+	public void verifyingLoginWithValidEmailAndInvalidPassword () {
+
+		driver.findElement(By.id("input-email")).sendKeys("amotooricap9@gmail.com");
+		driver.findElement(By.id("input-password")).sendKeys("ABC123345");
+		driver.findElement(By.xpath("//input[@value='Login']")).click();
+		
+		Assert.assertTrue(driver.findElement(By.xpath("//div[@class='alert alert-danger alert-dismissible']")).isDisplayed());
+		
+	}
+	@Test (priority=4)
+	public void verifyingLoginWithoutProvidingCredentials () {
+
+		driver.findElement(By.id("input-email")).sendKeys("");
+		driver.findElement(By.id("input-password")).sendKeys("");
+		driver.findElement(By.xpath("//input[@value='Login']")).click();
+		
+		Assert.assertTrue(driver.findElement(By.xpath("//div[@class='alert alert-danger alert-dismissible']")).isDisplayed());
+		
+	}
 }
  
